287075638aea97c011eade3e3bdab1648937e977
wenxu2 <wx2@students.uwf.edu>
2016-11-30 11:34:33 -0600

    Update bEncounter.java

--- bEncounter.java
+++ bEncounter.java
@@ -7,5 +7,21 @@ public class bEncounter
 	
 	public static void main(String[] a) 
 	{
+		public getfighter (String aFrighter)
+		{
+			return frighter;
+		}
+		public getitem (String aItem)
+		{
+			return item;
+		}
+		public getskill (String aSkill)
+		{
+			return skill;
+		}
+		public getenemy (String aEnemy)
+		{
+			return enemy;
+		}
 	}
 }
